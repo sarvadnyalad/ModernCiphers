b10af8bc1fbada6fa59d58d5a44e9830b674b91a
sarvadnyalad <sarvadnyal1234@gmail.com>
2025-07-01 11:10:15 +0100

    added encryption and decryption in fernet cipher algo

--- ciphers/fernet_handler.py
+++ ciphers/fernet_handler.py
@@ -1,6 +1,6 @@
 import os 
-from cryptography.fernet import Fernet
-from dotenv import load_dotenv
+from cryptography.fernet import Fernet # type: ignore
+from dotenv import load_dotenv # type: ignore
 
 load_dotenv()
 fernet = Fernet(os.getenv("FERNET_KEY"))    # Fernet key is mentioned in .env file
